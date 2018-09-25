a86c3f1063349fddbfba5bcb2e16149c04fe27f5
Gerardo Soto <gerardo.soto@janeirodigital.com>
2018-09-25 08:37:15 -0600

    XF-979 AUTO_TC 6.4.7 Update Logo & Styles of Tenant - Reset to Default - Tenant reset to default styles values
    Signed-off-by: Gerardo Soto <gerardo.soto@janeirodigital.com>

--- xform-automation-webdriver/xform-controllers/src/main/java/com/janeirodigital/xform/webdriver/actions/TenantsActions.java
+++ xform-automation-webdriver/xform-controllers/src/main/java/com/janeirodigital/xform/webdriver/actions/TenantsActions.java
@@ -133,4 +133,11 @@ public class TenantsActions {
         common.fillInput(selectors.txtBKColor,tcdata.getsBKColor());
         common.findAndClick(selectors.btnResetStyle);
     }
+
+    public void tenantResetStyles(TenantsFileData tcdata){
+        common.goAndFindTenant(tcdata.getsTenantName(),selectors.btnAddTenant,selectors.txtQuickSearch,selectors.txtQuickSearchField,selectors.btnQuickSearch,selectors.btnQuickSearchEdit);
+        common.findAndClick(selectors.txtLogoAndStyles);
+        common.waitForPresenceOfElement(1,selectors.btnResetStyle);
+        common.findAndClick(selectors.btnResetStyle);
+    }
 }
